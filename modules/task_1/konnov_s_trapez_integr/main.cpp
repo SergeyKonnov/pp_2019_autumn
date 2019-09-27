de0e2c1abfcbfae325ce6169146b0a9a7b8fd58c
Sergey Konnov <sergeykonnov007@yandex.ru>
2019-09-27 21:55:47 +0300

    add some tests

--- modules/task_1/konnov_s_trapez_integr/main.cpp
+++ modules/task_1/konnov_s_trapez_integr/main.cpp
@@ -2,9 +2,54 @@
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <mpi.h>
+#include <cmath>
+#include <functional>
 #include "./trapez_integr.h"
 
-// TESTS
+TEST(Trapez_Integr, Test_Sequential_Integral) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        std::function<double(double)>f = [](double x) {return x*x; };
+        double i1 = GetTrapezIntegrSequential(2, 5, 1, f);
+        double i2 = 39.5;
+        EXPECT_NEAR(i1, i2, 1e-9);
+    }
+}   
+
+TEST(Trapez_Integr, Test_Sequetional_Integral_On_Sin_Function) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        std::function<double(double)>f = [](double x) {return sin(x); };
+        double i1 = GetTrapezIntegrSequential(-10, 10, 0.5, f);
+        double i2 = 0;
+        EXPECT_NEAR(i1, i2, 1e-9);
+    }
+}
+
+TEST(Trapez_Integr, Test_Parallel_Integral) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::function<double(double)>f = [](double x) {return x * x; };
+    double i1 = GetTrapezIntegrParallel(2, 5, 3, f);
+    if (rank == 0) {
+        double i2 = GetTrapezIntegrSequential(2, 5, 1, f);
+        EXPECT_NEAR(i1, i2, 1e-9);
+    }
+}
+
+TEST(Trapez_Integr, Test_Parallel_Integral_On_Sin_Function) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::function<double(double)>f = [](double x) {return sin(x); };
+    double i1 = GetTrapezIntegrParallel(-10, 10, 20, f);
+    if (rank == 0) {
+        double i2 = GetTrapezIntegrSequential(-10, 10, 0.5, f);
+        EXPECT_NEAR(i1, i2, 1e-9);
+    }
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
--- modules/task_1/konnov_s_trapez_integr/trapez_integr.cpp
+++ modules/task_1/konnov_s_trapez_integr/trapez_integr.cpp
@@ -2,7 +2,7 @@
 #include <mpi.h>
 #include "../../../modules/task_1/konnov_s_trapez_integr/trapez_integr.h"
 #include <functional>
-
+#include <iostream>
 double GetTrapezIntegrParallel(int l, int r, int n, std::function<double(double)>& f) {
     
     const double step = double(r - l) / double(n);
@@ -10,7 +10,7 @@ double GetTrapezIntegrParallel(int l, int r, int n, std::function<double(double)
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     const int delta = (r - l) / rank;
     int left, right;
     
@@ -32,17 +32,19 @@ double GetTrapezIntegrParallel(int l, int r, int n, std::function<double(double)
     
     double local_integral = GetTrapezIntegrSequential(left, right, step, f);
     if (rank != 0)
-        local_integral += f(left);
+        local_integral += f(left)/2;
     if (rank != size - 1)
-        local_integral += f(right);
+        local_integral += f(right)/2;
     double global_integral = MPI_Reduce(&local_integral, &global_integral, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     return global_integral;
 }
 
 double GetTrapezIntegrSequential(int l, int r, double step, std::function<double(double)>& f) {
-    double integral = f(l) / 2 + f(r) / 2;
-    for (int i = l + 1; i < r - 1; i++)
+    double integral = f(double(l)) / 2 + f(double(r)) / 2;
+    double i = double(l) + step;
+    for (; i < double(r); i+=step) {
         integral += f(i);
-    integral *= step;
+    }
+    integral*=step;
     return integral;
 }
