04b00ec46fae0feba6f6e77f967236893f5e4208
Sergey Konnov <sergeyzorro007@gmail.com>
2019-11-12 21:57:16 +0300

    add whitespace, delete tabs, delete mpi_barrier

--- modules/task_2/konnov_s_matrix_mult_only_hor_a/matrix_mult_only_hor_a.h
+++ modules/task_2/konnov_s_matrix_mult_only_hor_a/matrix_mult_only_hor_a.h
@@ -3,7 +3,7 @@
 #define MODULES_TASK_1_KONNOV_S_MATRIX_MULT_ONLY_HOR_A_H_
 #include <vector>
 
-std::vector<int> matrix_mult_parallel(std::vector<int>& a, std::vector<int>&b, int msize);
+std::vector<int> matrix_mult_parallel(const std::vector<int>& aa, const std::vector<int>&bb, int msize);
 std::vector<int> matrix_mult_sequential(const std::vector<int>& a, const std::vector<int>&b, int msize);
 std::vector<int>generate_matrix(int size, int time);
 
